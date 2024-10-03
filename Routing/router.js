7905cecb607eebbf4c3b47762758f0ebd9885a61
Dhanish Ahamed <danishahamed.dk@gmail.com>
2024-10-03 21:43:36 +0530

    Adding general aptitude question(Admin) finished.

--- Routing/router.js
+++ Routing/router.js
@@ -3,14 +3,18 @@
 // 1)import Express
 const express = require('express');
 
-// import controller
+// import user controller
 const userController = require('../Controllers/usercontrollers')
+
+// import general controller'
+const generalController = require('../Controllers/generalController')
 
 // 2)create an object for Router() class in the express module
 const router = new express.Router();
 
 // 3)Logic
     // Syntax = router.httpreq(path,()=>{how to solve})
+    
     // a)Register
     router.post('/user/register',userController.register)
 
@@ -19,6 +23,9 @@ const router = new express.Router();
 
     //c)Admin Login
     router.post('/admin/login',userController.adminlogin)
+
+    //d)Admin Add questions
+    router.post('/general/pot/add',generalController.addQuestion)
 
 // 4)Export router
 module.exports = router
