28c7f5e76e70185efc6b6af0b12d78c384c0b5bc
Dhanish Ahamed <danishahamed.dk@gmail.com>
2024-11-12 02:23:42 +0400

    Update profile completed - error resolving pending

--- Middlewares/jwtMiddleware.js
+++ Middlewares/jwtMiddleware.js
@@ -1,6 +1,6 @@
 //this is a middleware (router middleware) 
 //a middleware have the pottential to control the request response cycle.
-// two types - application middleware - affecting an entire app ie, all the requests pass throught this type of middlewares
+// two types - application middleware - affecting an entire app ie; all the requests pass throught this type of middlewares
 //           - router middleware - Only present at a particular path (Middleware using in this project)
 
 //import jwt
